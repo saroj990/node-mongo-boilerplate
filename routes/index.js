c241de31acd29bc2c52a2c3582fa4ea7bae1063c
saroj990 <sarojsasmal990@gmail.com>
2017-02-22 21:34:30 +0530

    Added dashboard page

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/dashboard', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
